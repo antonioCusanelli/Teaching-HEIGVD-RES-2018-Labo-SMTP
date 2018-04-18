e72a5b7b30248cd55e912625c146cd94258c096c
Basler Corentin <corentin.basler@heig-vd.ch>
2018-04-18 09:38:43 +0200

    Implémentation Group.java, Message.java, Person.java et Prank.java

--- src/model/mail/Group.java
+++ src/model/mail/Group.java
@@ -1,4 +1,16 @@
 package model.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+    private List<Person> members = new ArrayList<>();
+
+    public void addMember(Person p){
+        members.add(p);
+    }
+
+    public List<Person> getMembers(){
+        return members;
+    }
 }
--- src/model/prank/Prank.java
+++ src/model/prank/Prank.java
@@ -1,4 +1,47 @@
 package model.prank;
 
+import model.mail.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prank {
+
+    private Person victimSender;
+    private List<Person> victimRecipient = new ArrayList<>();
+    private List<Person> witnessRecipient = new ArrayList<>();
+    private String message;
+
+
+    public Person getVictimSender() {
+        return victimSender;
+    }
+
+    public List<Person> getVictimRecipient() {
+        return victimRecipient;
+    }
+
+    public List<Person> getWitnessRecipient() {
+        return witnessRecipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setVictimSender(Person victimSender) {
+        this.victimSender = victimSender;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void addVictimRecipient(Person p){
+        victimRecipient.add(p);
+    }
+
+    public void addWitnessRecipient(Person p){
+        witnessRecipient.add(p);
+    }
 }
